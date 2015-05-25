d36f0324a5418b00afe8f333a17e1107e9eb40bf
Andreas Gillström <andreasgillstrom@gmail.com>
2015-05-25 20:46:24 +0200

    Tweak regex

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 'use strict';
-module.exports = typeof navigator !== 'undefined' && /Firefox\/[\d\.]+.*/.test(navigator.userAgent);
+module.exports = typeof navigator !== 'undefined' && /^(?!.*Seamonkey)(?=.*Firefox).*/i.test(navigator.userAgent);
